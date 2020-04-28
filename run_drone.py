0abd8d74f4ffea5d9157d64ca1b8b3ebc58af8fe
shawnwu4mac <shawnishere0806>
2020-04-28 20:01:19 +0800

    :beers: modify send to gateway detail

--- run_drone.py
+++ run_drone.py
@@ -1,31 +1,23 @@
 from ArdupilotEntity import ArdupilotEntity
 from EKS76S import EKS76S
 from time import sleep
-import pigpio
 
 
-# pixhawk = ArdupilotEntity("/dev/ttyAMA0", 57600, 30)
-# drone_data = pixhawk.get_drone_data()
-# print(drone_data)
+pixhawk = ArdupilotEntity("/dev/ttyAMA0", 57600, 30)
+drone_data = pixhawk.get_drone_data()
+print(drone_data)
 
-
-another_tx = 36
-pi = pigpio.pi('soft', 8888)
-pi.set_mode(another_tx, pigpio.OUTPUT)
-
-
-lora_board = EKS76S("/dev/ttyAMA1", 57600, 10)
+lora_board = EKS76S('/dev/ttyUSB0', 115200, 5)
 
 if lora_board.join_abp() is False:
     lora_board.join_abp()
 
-result = lora_board.send_data_to_gateway("123456")
 
 # print("Drone status")
 # print(pixhawk.vehicle.is_armable)
-# while True:
-#     result = lora_board.send_data_to_gateway(pixhawk.get_drone_data())
-#     print("-------->")
-#     print(result)
-#     sleep(3)
+while True:
+    result = lora_board.send_data_to_gateway(pixhawk.get_drone_data())
+    print("-------->")
+    print(result)
+    sleep(3)
 
