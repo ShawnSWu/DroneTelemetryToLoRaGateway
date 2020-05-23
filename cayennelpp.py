afcc254d1abd847c77f6fa24d00930dc856567c3
shawnwu4mac <shawnishere0806>
2020-05-23 21:34:57 +0800

    :beers: modify send to gateway detail

--- cayennelpp.py
+++ cayennelpp.py
@@ -147,6 +147,6 @@ class CayenneLPP(object):
         else:
             return self._add_to_buffer('LPP_ATTITUDE',
                                        channel,
-                                       round(pitch, 8) * 1000,
-                                       round(yaw, 8) * 10000,
-                                       round(roll, 8) * 1000)
+                                       round(pitch, 3) * 1000,
+                                       round(yaw, 3) * 10000,
+                                       round(roll, 3) * 1000)
