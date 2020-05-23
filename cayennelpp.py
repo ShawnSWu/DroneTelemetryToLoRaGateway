29d6658a0d29ba23a7f2f014cc3877395667aac1
shawnwu4mac <shawnishere0806>
2020-05-23 21:28:26 +0800

    :beers: modify send to gateway detail

--- cayennelpp.py
+++ cayennelpp.py
@@ -12,7 +12,7 @@ TYPE = {
     'LPP_GYROMETER': [3334, (2, 2, 2)],
     'LPP_GPS': [3336, (3, 3, 3)],
     'LPP_NED': [3337, (2, 2, 2)],
-    'LPP_ATTITUDE': [3338, (2, 2, 2)]
+    'LPP_ATTITUDE': [3338, (3, 3, 3)]
 }
 
 
@@ -147,6 +147,6 @@ class CayenneLPP(object):
         else:
             return self._add_to_buffer('LPP_ATTITUDE',
                                        channel,
-                                       round(pitch, 8) * 10000,
+                                       round(pitch, 8) * 1000,
                                        round(yaw, 8) * 10000,
-                                       round(roll, 8) * 10000)
+                                       round(roll, 8) * 1000)
