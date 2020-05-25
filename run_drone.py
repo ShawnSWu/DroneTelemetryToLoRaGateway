28afd003f5288aa05ef0681ff1ed87ce7f6029a8
shawnwu4mac <shawnishere0806>
2020-05-25 17:00:09 +0800

    :beers: modify send to gateway detail

--- run_drone.py
+++ run_drone.py
@@ -9,15 +9,17 @@ lora_board = EKS76S('/dev/ttyUSB0', 115200, 5)
 if lora_board.join_abp() is False:
     lora_board.join_abp()
 
-while True:
-    print("<---------------Waiting Drone take off--------------->")
-    while pixhawk.vehicle.armed:
-        if lora_board.join_abp() is False:
-            lora_board.join_abp()
-        result = lora_board.send_data_to_gateway(pixhawk.get_drone_data())
-        print("-------->")
-        print(result)
-        sleep(2)
+result = lora_board.send_data_to_gateway( pixhawk.get_drone_data() )
 
-    sleep(2)
+# while True:
+#     print("<---------------Waiting Drone take off--------------->")
+#     while pixhawk.vehicle.armed:
+#         if lora_board.join_abp() is False:
+#             lora_board.join_abp()
+#         result = lora_board.send_data_to_gateway(pixhawk.get_drone_data())
+#         print("-------->")
+#         print(result)
+#         sleep(2)
+#
+#     sleep(2)
 
