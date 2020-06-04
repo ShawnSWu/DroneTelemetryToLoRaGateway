321d9b93137a5f9bbd3689329bf4ca471a2160fd
shawnwu4mac <shawnishere0806>
2020-06-05 03:42:59 +0800

    :beers: modify send to gateway detail

--- CollectDroneDataToFile.py
+++ CollectDroneDataToFile.py
@@ -6,17 +6,18 @@ from datetime import datetime
 
 pixhawk = ArdupilotEntity("/dev/ttyAMA0", 57600, 30)
 
-today = str(date.today())
-time = str(datetime.now().strftime("%H:%M"))
-file_name = today + '-' + time
-extension = ".csv"
 
 while True:
-    with open( "TrajectoryData/" + file_name + extension, "w+" ) as file:
-        while True:
+    print("<---------------Waiting Drone take off--------------->")
+    if pixhawk.vehicle.armed:
+        today = str( date.today() )
+        time = str( datetime.now().strftime( "%H:%M" ) )
+        file_name = today + '-' + time
+        extension = ".csv"
+        with open( "TrajectoryData/" + file_name + extension, "w+" ) as file:
             writer = csv.writer( file )
-            row_data = pixhawk.get_row_data()
-            writer.writerow( row_data )
-            print(row_data)
-            sleep( 2 )
-
+            while pixhawk.vehicle.armed:
+                row_data = pixhawk.get_row_data()
+                writer.writerow( row_data )
+                print(row_data)
+                sleep( 2 )
